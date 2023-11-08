554179e909927ceed3dfd39c422db2b97e6739c4
razibraj <razibraj56@gmail.com>
2023-11-09 01:48:56 +0600

    added the filter

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,24 +26,64 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    
+
 
     const roomCollection = client.db('hotelRoom').collection('room');
-    
+
     const availableRoomsCollection = client.db('hotelRoom').collection('availableRooms');
 
-    app.get('/room', async(req, res) => {
-        const cursor = roomCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/room', async (req, res) => {
+      const cursor = roomCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
-    app.get('/room', async(req, res) => {
-        const cursor = availableRoomsCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/availableRooms', async (req, res) => {
+      const cursor = availableRoomsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
+
+    app.get('/availableRooms/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await availableRoomsCollection.findOne(query)
+      res.send(result)
+    })
+
     
+    app.get('/sortedPrice/:id', async (req, res) => {
+      const sortedValue = req.params.id;
+      console.log(sortedValue);
+      if(sortedValue === 'low'){
+        const products = await availableRoomsCollection.find().sort({ price: 1 }).toArray();
+        res.json(products);
+        return;
+
+      }
+      else if (sortedValue == 'high'){
+        const products = await availableRoomsCollection.find().sort({ price: -1 }).toArray();
+        res.json(products);
+        return;
+      }
+      else{
+        const products = await availableRoomsCollection.find().toArray();
+        res.json(products);
+        return;
+      }
+     
+    });
+
+    // Route to get six products sorted by price in descending order
+    app.get('/descending', async (req, res) => {
+      try {
+        const products = await Product.find().sort({ price: -1 }).limit(6);
+        res.json(products);
+      } catch (error) {
+        res.status(500).json({ error: 'An error occurred' });
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -58,9 +98,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('hotel is running')
+  res.send('hotel is running')
 })
 
 app.listen(port, () => {
-    console.log(`hotel room server is running on port ${port}`);
+  console.log(`hotel room server is running on port ${port}`);
 })
